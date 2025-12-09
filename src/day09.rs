e4db87b93e8da46899c23da0c1e237435075873d
meltinglava <meltinglavaoutland@gmail.com>
2025-12-09 17:56:15 +0100

    d09 p2 complete

--- src/day09.rs
+++ src/day09.rs
@@ -10,6 +10,7 @@ use nom::{
 use crate::convert_iresult_to_owned;
 
 type AocType = (usize, usize); // factor, value
+type Edge = (AocType, AocType);
 
 #[aoc_generator(day09)]
 pub fn input_generator(input: &str) -> Result<Vec<AocType>, nom::Err<nom::error::Error<String>>> {
@@ -41,66 +42,44 @@ fn part1(input: &[AocType]) -> usize {
         .unwrap()
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Edge {
-    edge: [AocType; 2],
-}
+fn is_fully_contained(edges: &[Edge], corner1: AocType, corner2: AocType) -> bool {
+    let x_min = corner1.0.min(corner2.0);
+    let x_max = corner1.0.max(corner2.0);
+    let y_min = corner1.1.min(corner2.1);
+    let y_max = corner1.1.max(corner2.1);
 
-fn ccw(a: AocType, b: AocType, c: AocType) -> bool {
-    let a_x = a.0 as isize;
-    let a_y = a.1 as isize;
-    let b_x = b.0 as isize;
-    let b_y = b.1 as isize;
-    let c_x = c.0 as isize;
-    let c_y = c.1 as isize;
-    (c_y - a_y) * (b_x - a_x) > (b_y - a_y) * (c_x - a_x)
-}
-
-fn intersect(a: &Edge, b: &Edge) -> bool {
-    ccw(a.edge[0], b.edge[0], b.edge[1]) != ccw(a.edge[1], b.edge[0], b.edge[1])
-        && ccw(a.edge[0], a.edge[1], b.edge[0]) != ccw(a.edge[0], a.edge[1], b.edge[1])
+    for edge in edges {
+        let edge_x_min = edge.0 .0.min(edge.1 .0);
+        let edge_x_max = edge.0 .0.max(edge.1 .0);
+        let edge_y_min = edge.0 .1.min(edge.1 .1);
+        let edge_y_max = edge.0 .1.max(edge.1 .1);
+        if x_min < edge_x_max && x_max > edge_x_min && y_min < edge_y_max && y_max > edge_y_min {
+            return false;
+        }
+    }
+    true
 }
 
 #[aoc(day09, part2)]
 fn part2(red_tiles: &[AocType]) -> usize {
-    let mut edges = red_tiles
-        .windows(2)
-        .map(|f| Edge { edge: [f[0], f[1]] })
-        .collect_vec();
+    let mut edges = red_tiles.windows(2).map(|w| (w[0], w[1])).collect_vec();
 
-    edges.push(Edge {
-        edge: [red_tiles[red_tiles.len() - 1], red_tiles[0]],
-    });
+    edges.push((red_tiles[red_tiles.len() - 1], red_tiles[0]));
 
-    let mut max = 0;
-    red_tiles.iter().combinations(2).for_each(|connection| {
-        let area = squared_area(connection[0], connection[1]);
-        if area <= max {
-            return;
+    let mut result = 0;
+
+    for rectangle in red_tiles.iter().combinations(2) {
+        let area = squared_area(rectangle[0], rectangle[1]);
+        if area <= result {
+            continue;
         }
-        let connection_edges = [
-            Edge {
-                edge: [*connection[0], (connection[0].0, connection[1].1)],
-            },
-            Edge {
-                edge: [(connection[0].0, connection[1].1), *connection[1]],
-            },
-            Edge {
-                edge: [*connection[1], (connection[1].0, connection[0].1)],
-            },
-            Edge {
-                edge: [(connection[1].0, connection[0].1), *connection[0]],
-            },
-        ];
-        if !edges
-            .iter()
-            .any(|e| connection_edges.iter().any(|ce| intersect(e, ce)))
-        {
-            max = area;
-        }
-    });
 
-    max
+        if is_fully_contained(&edges, *rectangle[0], *rectangle[1]) {
+            result = area;
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -118,55 +97,6 @@ mod tests {
         7,3
     "};
 
-    const TEST_INPUT2: &str = indoc! {"
-        1,0
-        3,0
-        3,6
-        16,6
-        16,0
-        18,0
-        18,9
-        13,9
-        13,7
-        6,7
-        6,9
-        1,9
-    "};
-
-    #[test]
-    fn test_intersect() {
-        let edge1 = Edge {
-            edge: [(1, 1), (4, 4)],
-        };
-        let edge2 = Edge {
-            edge: [(1, 4), (4, 1)],
-        };
-        let edge3 = Edge {
-            edge: [(5, 5), (6, 6)],
-        };
-        let edge4 = Edge {
-            edge: [(1, 1), (1, 2)],
-        };
-        let edge5 = Edge {
-            edge: [(0, 3), (3, 3)],
-        };
-        assert!(intersect(&edge1, &edge2));
-        assert!(!intersect(&edge1, &edge3));
-        assert!(!intersect(&edge1, &edge1));
-        assert!(!intersect(&edge4, &edge5));
-    }
-
-    #[test]
-    fn test_intersect2() {
-        let edge1 = Edge {
-            edge: [(1, 1), (5, 4)],
-        };
-        let edge2 = Edge {
-            edge: [(1, 7), (9, 1)],
-        };
-        assert!(!intersect(&edge1, &edge2));
-    }
-
     #[test]
     fn test_p1() {
         assert_eq!(part1(&input_generator(TEST_INPUT).unwrap()), 50);
@@ -175,10 +105,5 @@ mod tests {
     #[test]
     fn test_p2() {
         assert_eq!(part2(&input_generator(TEST_INPUT).unwrap()), 24);
-    }
-
-    #[test]
-    fn test_p2_2() {
-        assert_eq!(part2(&input_generator(TEST_INPUT2).unwrap()), 30);
     }
 }
