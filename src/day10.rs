9dfb6e4f0a31f7327edb212c110e104615554780
meltinglava <meltinglavaoutland@gmail.com>
2025-12-11 22:36:03 +0100

    day10 finished

--- src/day10.rs
+++ src/day10.rs
@@ -1,3 +1,5 @@
+use good_lp::{microlp, variable, variables, Expression, Solution, SolverModel};
+use itertools::Itertools;
 use nom::{
     branch::alt,
     character::complete::{self, space1},
@@ -67,7 +69,25 @@ fn input_generator(input: &str) -> Result<Vec<AocType>, nom::Err<nom::error::Err
 }
 
 fn min_presses_to_goal(aoc: &AocType) -> usize {
-    todo!()
+    if aoc.goal.iter().all(|&b| !b) {
+        return 0;
+    }
+    for choices in 1..=aoc.buttons.len() {
+        for button_set in aoc.buttons.iter().combinations(choices) {
+            let mut lights = vec![false; aoc.goal.len()];
+            for button in button_set {
+                for &pos in button {
+                    if pos < lights.len() {
+                        lights[pos] = !lights[pos];
+                    }
+                }
+            }
+            if lights == aoc.goal {
+                return choices;
+            }
+        }
+    }
+    unreachable!()
 }
 
 #[aoc(day10, part1)]
@@ -75,10 +95,84 @@ fn part1(input: &[AocType]) -> usize {
     input.iter().map(min_presses_to_goal).sum()
 }
 
-// #[aoc(day10, part2)]
-// fn part2(input: &[AocType]) -> usize {
-//    todo!()
+fn min_presses_to_joltage(aoc: &AocType) -> usize {
+    let mut vars = variables!();
+    let presses = (0..aoc.buttons.len())
+        .map(|_| vars.add(variable().min(0).integer()))
+        .collect_vec();
+
+    let total_presses: Expression = presses.iter().sum();
+
+    let mut problem = vars.minimise(total_presses).using(microlp);
+
+    // for each jolt counter, sum of relevant presses must equal the target joltage
+    for (jolt_idx, &target) in aoc.joltage.iter().enumerate() {
+        let mut expr = Expression::from(0.0);
+
+        for (btn_idx, relevant_idxs) in aoc.buttons.iter().enumerate() {
+            // if button is relevant, add its press variable to the constraint
+            if relevant_idxs.contains(&jolt_idx) {
+                expr += presses[btn_idx];
+            }
+        }
+
+        // sum of relevant presses == target joltage
+        problem.add_constraint(expr.eq(target as f64));
+    }
+
+    let solution = problem.solve().unwrap();
+
+    presses
+        .iter()
+        .map(|v| solution.value(*v).round() as usize)
+        .sum()
+}
+
+// fn min_presses_to_joltage(aoc: &AocType) -> usize {
+//     let button_ints = (0..aoc.buttons.len())
+//         .map(|i| Int::new_const(format!("b{}", i)))
+//         .collect_vec();
+
+//     let targets: Vec<Vec<usize>> = (0..aoc.joltage.len())
+//         .map(|i| {
+//             aoc.buttons
+//                 .iter()
+//                 .enumerate()
+//                 .filter_map(|(j, btn)| if btn.contains(&i) { Some(j) } else { None })
+//                 .collect()
+//         })
+//         .collect();
+
+//     let solver = Solver::new();
+//     for (target, joltage) in targets.iter().zip(aoc.joltage.iter()) {
+//         // sum for all buttons controlling a joltage asserts eq to joltage target
+//         let s: Int = target.iter().filter_map(|&btn_idx| button_ints.get(btn_idx)).sum();
+//         solver.assert(s.eq(*joltage as u64))
+//     }
+//     for b in &button_ints {
+//         solver.assert(b.ge(&Int::from_i64(0)));
+//     }
+//     for (int, button) in button_ints.iter().zip(aoc.buttons.iter()) {
+//         let min = button.iter().map(|i| aoc.joltage[*i]).min().unwrap();
+//         solver.assert(int.le(min as u64));
+//     }
+//     let optimizer = Optimize::new();
+//     optimizer.minimize(&button_ints.iter().sum::<Int>());
+//     Model::of_optimize(&optimizer).unwrap();
+
+//     let mut min = usize::MAX;
+//     for solve in solver.solutions(button_ints, false).take(100) {
+//         let sum: u64= solve.iter().map(Int::as_u64).map(|v| v.unwrap()).sum();
+//         min = min.min(sum as usize);
+//     }
+
+//     min
 // }
+
+#[aoc(day10, part2)]
+fn part2(input: &[AocType]) -> usize {
+    input.iter().map(min_presses_to_joltage).sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -107,5 +201,18 @@ mod tests {
         assert_eq!(min_presses_to_goal(&inputs[0]), 2);
         assert_eq!(min_presses_to_goal(&inputs[1]), 3);
         assert_eq!(min_presses_to_goal(&inputs[2]), 2);
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(part2(&input_generator(TEST_INPUT).unwrap()), 33);
+    }
+
+    #[test]
+    fn test_each_input_joltage() {
+        let inputs = input_generator(TEST_INPUT).unwrap();
+        assert_eq!(min_presses_to_joltage(&inputs[0]), 10);
+        assert_eq!(min_presses_to_joltage(&inputs[1]), 12);
+        assert_eq!(min_presses_to_joltage(&inputs[2]), 11);
     }
 }
